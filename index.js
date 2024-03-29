dfbc1d346b989c758aca91881f7761751c082092
Dheeraj0408 <dheeraj4f0@gmail.com>
2024-03-29 11:22:25 +0530

    Edit-4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json"); // assuming db.json is in the same directory
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 server.use(middlewares);
 server.use(router);
